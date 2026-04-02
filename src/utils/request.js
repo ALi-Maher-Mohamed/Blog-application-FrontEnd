568f40a6e608c7edcada5d6abed3aa6888996dc3
AliMaher <ali.maher0013@gmail.com>
2026-04-02 21:47:34 +0200

    fix some bugs

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const request = axios.create({
-  baseURL: "https://blog-application-backend-green.vercel.app/",
+  baseURL: "https://blog-application-backend-green.vercel.app",
 });
 
 export default request;
